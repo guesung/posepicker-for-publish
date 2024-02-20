5df0748db8426fbfbbe6ac737229b16dd296801c
guesung <gueit214@naver.com>
2024-02-20 17:41:35 +0000

    🏗 folder :  redirect uri 드디어 간단하게 변경!

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
+
+import { getRegister } from '@/apis';
+import useUserState from '@/context/userState';
+
+export default function Page() {
+  const code = useSearchParams().get('code');
+  const router = useRouter();
+  const { setUser } = useUserState();
+
+  useEffect(() => {
+    if (code) {
+      getRegister(code).then((response) => setUser(response));
+      router.replace('/menu');
+    }
+  });
+
+  return <>Loading...</>;
+}
--- src/context/userState.tsx
+++ src/context/userState.tsx
@@ -1,5 +1,24 @@
-import { atom } from 'recoil';
+import { atom, useRecoilValue, useSetRecoilState } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
 
 import { RegisterResponse } from '@/apis';
 
-export const userAtom = atom<RegisterResponse>({ key: 'user' });
+const { persistAtom } = recoilPersist();
+
+const userAtom = atom<RegisterResponse>({
+  key: 'user',
+  default: undefined,
+  effects_UNSTABLE: [persistAtom],
+});
+
+export default function useUserState() {
+  const getUserAtom = useRecoilValue(userAtom);
+  const setUserAtom = useSetRecoilState(userAtom);
+
+  const isLogin = getUserAtom !== undefined;
+  const userData = getUserAtom;
+  const accessToken = () => getUserAtom.token.accessToken;
+  const setUser = (data: RegisterResponse) => setUserAtom(data);
+
+  return { isLogin, userData, accessToken, setUser };
+}
