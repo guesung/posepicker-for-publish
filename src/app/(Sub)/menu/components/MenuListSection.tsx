4e73b6538c7a74f97a38a7fca0994dbc621501a0
guesung <gueit214@naver.com>
2024-03-19 19:13:20 +0000

    🏗 folder :  menuList -> data.ts

--- src/app/(Sub)/menu/components/MenuListSection.tsx
+++ src/app/(Sub)/menu/components/MenuListSection.tsx
@@ -1,14 +1,43 @@
 'use client';
 
-import MenuList from '@/constants/menuList';
+import { patchLogout } from '@/apis';
+import { PrimaryButton } from '@/components/Button';
+import { Popup } from '@/components/Modal';
+import { useOverlay } from '@/components/Overlay/useOverlay';
+import { menuList } from '@/constants/data';
 import useUserState from '@/context/userState';
 
 export default function MenuListSection() {
-  const { isLogin } = useUserState();
+  const { isLogin, accessToken, clearUser } = useUserState();
+  const { open } = useOverlay();
+
+  function logout() {
+    if (accessToken) {
+      clearUser();
+      patchLogout(accessToken, accessToken);
+    }
+  }
+  function handleLogout() {
+    open(({ exit }) => (
+      <Popup
+        title="로그아웃"
+        content={`북마크는 로그인 시에만 유지되어요.\n정말 로그아웃하시겠어요?`}
+      >
+        <>
+          <PrimaryButton text={'로그아웃'} onClick={logout} type="secondary" />
+          <PrimaryButton text="로그인 유지" onClick={exit} />
+        </>
+      </Popup>
+    ));
+  }
+
+  function handleDeleteAccount() {
+    clearUser();
+  }
 
   return (
     <section className="flex flex-col">
-      {MenuList.map((item, idx) =>
+      {menuList.map((item, idx) =>
         item.text ? (
           <div
             key={idx}
@@ -23,10 +52,10 @@ export default function MenuListSection() {
       )}
       {isLogin && (
         <>
-          <div className="cursor-pointer py-12" onClick={() => console.log('로그아웃')}>
+          <div className="cursor-pointer py-12" onClick={() => handleLogout()}>
             <span id="subtitle-1">로그아웃</span>
           </div>
-          <div className="cursor-pointer py-12" onClick={() => console.log('회원탈퇴')}>
+          <div className="cursor-pointer py-12" onClick={() => handleDeleteAccount()}>
             <span id="subtitle-1" className="text-tertiary">
               탈퇴하기
             </span>
